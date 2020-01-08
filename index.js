f809bfca66b17dfd815348b1e7861e8cfd37d71e
Alex Trebble <alextrebble@gmail.com>
2020-01-08 17:46:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello" + specialGuest + "Let's talk about space";
+var greetSpecialGuest = "Greet" + specialGuest + "Let's talk about space";
 
 var topic = "space";
 
